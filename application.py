b41fe2933267b65304c9f247a24078a950ec15f2
Muhammad Abd-Elsattar <62842603+mmuhhammadd@users.noreply.github.com>
2021-08-19 19:39:33 +0200

    Update application.py

--- application.py
+++ application.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USER"] = os.getenv("MAIL_USERNAME")
-app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_USERNAME")
-app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_USER"] = "mmuhhhammadd@gmail.com"
+app.config["MAIL_DEFAULT_SENDER"] = "mmuhhhammadd@gmail.com"
+app.config["MAIL_PASSWORD"] = "sekolana11011899"
 mail = Mail(app)
 
 @app.route("/", methods=["GET", "POST"])
